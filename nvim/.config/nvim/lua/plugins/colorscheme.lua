2f545f85e743964aefc46e1962eb9810780d5544
Dennis Sonntag <sonntagdennis8@gmail.com>
2023-04-06 20:39:20 -0600

    nvim highlight

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -14,8 +14,12 @@ return {
 				floats = "transparent", -- style for floating windows
 			},
 		})
-
 		vim.cmd.colorscheme("tokyonight")
+
+		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffffff", bold = true })
+
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "#555f8c" })
+		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2d3349", bold = true })
 
 		vim.api.nvim_set_hl(0, "MatchParen", {
 			bg = "#3b4261",
