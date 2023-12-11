f4c95eeafbcaf9115dbd0b45d0f2500b30e4ab78
Dennis Sonntag <sonntagdennis8@gmail.com>
2023-12-11 12:33:31 -0700

    emacs init file

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,14 @@
+(load "~/.config/emacs/init.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-mode visual-fill-column org-bullets evil-collection evil general doom-themes helpful which-key rainbow-delimiters doom-modeline all-the-icons ivy-rich counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
