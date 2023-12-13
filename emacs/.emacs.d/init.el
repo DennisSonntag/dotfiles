ae96538c7045406df02e1dbe13496453757cf47f
Dennis Sonntag <sonntagdennis8@gmail.com>
2023-12-12 20:49:18 -0700

    BEANS

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,14 +1,1 @@
 (load "~/.config/emacs/init.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(electric-pairs-mode cape which-key vterm visual-fill-column tree-sitter-langs rainbow-delimiters org-bullets ivy-rich helpful general evil-nerd-commenter evil-collection doom-themes doom-modeline dired-single dired-open counsel corfu all-the-icons-dired)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
