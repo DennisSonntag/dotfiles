6a75caffa9d05ece95d9371407d2d19cda6801b8
Dennis Sonntag <sonntagdennis8@gmail.com>
2024-03-26 13:31:52 -0600

    fixed fold symbol in statuscol

--- nvim/.config/nvim/lua/plugins/fold.lua
+++ nvim/.config/nvim/lua/plugins/fold.lua
@@ -1,46 +1,3 @@
--- return {
--- 	"luukvbaal/statuscol.nvim",
--- 	event = "BufRead",
--- 	opts = function()
--- 		local icons = require("config.icons")
--- 		local signs = {
--- 			Error = icons.diagnostics.error,
--- 			Warn = icons.diagnostics.warn,
--- 			Hint = icons.diagnostics.hint,
--- 			Info = icons.diagnostics.info
--- 		}
---
--- 		for type, icon in pairs(signs) do
--- 			local hl = "DiagnosticSign" .. type
--- 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
--- 		end
---
--- 		return {
--- 			segments = {
--- 				{ text = { "%s" } },
--- 				{
--- 					text = { function(args)
--- 						if args.relnum == 0 then
--- 							if args.lnum == 69 then
--- 								return "nice"
--- 							elseif args.lnum == 420 then
--- 								return "blaze"
--- 							else
--- 								return args.lnum
--- 							end
--- 						else
--- 							return args.relnum
--- 						end
--- 					end },
--- 					click = "v:lua.ScLa",
--- 				},
--- 				{ text = { " " } },
---
--- 			}
--- 		}
--- 	end,
--- }
-
 return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = {
@@ -55,8 +12,16 @@ return {
 			setopt = true,
 			relculright = true,
 			segments = {
-
-				{ text = { builtin.foldfunc, " " }, click = "v:lua.ScFa", hl = "Comment" },
+				{
+					text = { function(args)
+						args.fold.sep = " ";
+						args.fold.close = "" -- foldclose
+						args.fold.open = "" -- foldopen
+						return builtin.foldfunc(args)
+					end, " " },
+					click = "v:lua.ScFa",
+					hl = "Comment"
+				},
 
 				{ text = { "%s" },                  click = "v:lua.ScSa" },
 				{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
