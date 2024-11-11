273e1d26bc7c313afbcae73a24f3e2b6fe970c34
Dennis Sonntag <sonntagdennis8@gmail.com>
2024-11-11 14:35:41 -0700

    started porting old nvim config

---
+++ kickstartNixCats/lua/kickstart/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+return {
+  'ThePrimeagen/harpoon',
+  keys = function()
+    local ui_status, ui = pcall(require, 'harpoon.ui')
+    if not ui_status then
+      return
+    end
+
+    local mark_staus, mark = pcall(require, 'harpoon.mark')
+    if not mark_staus then
+      return
+    end
+
+    local function mark_file()
+      mark.add_file()
+      vim.notify '󱡅  marked file'
+    end
+
+    local keymaps = {
+      { '<leader>a', mark_file },
+      { '<C-e>', ui.toggle_quick_menu },
+    }
+    for i = 1, 5 do
+      table.insert(keymaps, {
+        '<A-' .. i .. '>',
+        function()
+          ui.nav_file(i)
+        end,
+      })
+    end
+
+    return keymaps
+  end,
+}
---
+++ kickstartNixCats/lua/kickstart/plugins/telescope.lua
@@ -0,0 +1,146 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  cmd = 'Telescope',
+  keys = {
+    '<leader>f.',
+    '<leader>fh',
+    '<leader>fk',
+    '<leader>ff',
+    '<leader>fs',
+    '<leader>fw',
+    '<leader>fg',
+    '<leader>fd',
+    '<leader>fr',
+    '<leader>fb',
+    '<leader>/',
+    '<leader>s/',
+    '<leader>sn',
+  },
+  dependencies = {
+    'nvim-telescope/telescope-ui-select.nvim',
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
+  },
+  config = function()
+    local icons = require 'config.icons'
+    local builtin = require 'telescope.builtin'
+
+    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require('telescope').load_extension, 'ui-select')
+
+    local function filenameFirst(_, path)
+      local tail = vim.fs.basename(path)
+      local parent = vim.fs.dirname(path)
+      if parent == '.' then
+        return tail
+      end
+      return string.format('%s\t\t%s', tail, parent)
+    end
+
+    require('telescope').setup {
+      defaults = {
+        prompt_prefix = icons.ui.Telescope .. ' ',
+        selection_caret = icons.ui.Forward .. ' ',
+        entry_prefix = '   ',
+        initial_mode = 'insert',
+        selection_strategy = 'reset',
+        path_display = { 'tail' },
+        color_devicons = true,
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+          '--glob=!.git/',
+        },
+      },
+      pickers = {
+        live_grep = {
+          theme = 'dropdown',
+        },
+
+        grep_string = {
+          theme = 'dropdown',
+        },
+
+        find_files = {
+          theme = 'dropdown',
+          previewer = false,
+          path_display = filenameFirst,
+        },
+
+        buffers = {
+          theme = 'dropdown',
+          previewer = false,
+          initial_mode = 'normal',
+        },
+
+        lsp_references = {
+          theme = 'dropdown',
+          initial_mode = 'normal',
+        },
+
+        lsp_definitions = {
+          theme = 'dropdown',
+          initial_mode = 'normal',
+        },
+
+        lsp_declarations = {
+          theme = 'dropdown',
+          initial_mode = 'normal',
+        },
+
+        lsp_implementations = {
+          theme = 'dropdown',
+          initial_mode = 'normal',
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+        },
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown(),
+        },
+      },
+    }
+
+    vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+    vim.keymap.set('n', '<leader>fs', builtin.spell_suggest, { desc = '[F]ind [S]pelling' })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+    vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
+    vim.keymap.set('n', '<leader>/', function()
+      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 0,
+        previewer = false,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
+    vim.keymap.set('n', '<leader>s/', function()
+      builtin.live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+      }
+    end, { desc = '[S]earch [/] in Open Files' })
+    vim.keymap.set('n', '<leader>sn', function()
+      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+    end, { desc = '[S]earch [N]eovim files' })
+  end,
+}
