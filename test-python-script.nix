c554aec2d323395b013d70e0eb8eeed93ad27e53
Dennis Sonntag <sonntagdennis8@gmail.com>
2024-12-02 14:00:07 -0700

    thing

--- test-python-script.nix
+++ test-python-script.nix
@@ -1,4 +1,8 @@
 {pkgs}:
-pkgs.python3.pkgs.toPythonModule (pkgs.writePython "test-python-script" ''
+# pkgs.python3.pkgs.toPythonModule (pkgs.writePython "test-python-script" ''
+pkgs.python3.pkgs.toPythonModule (pkgs.writers.writePython3 "test-python-script" {} ''
   print("hello fucking world")
 '')
+    # (pkgs.writers.writePython3 "test-python-script" { } ''
+    #     print("hello fucking world")
+    #   '')
