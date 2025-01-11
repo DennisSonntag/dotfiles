bab194fa1a093e344c9d8f5ffa0a33846c465a42
Dennis Sonntag <sonntagdennis8@gmail.com>
2025-01-11 10:15:17 -0700

    updated blink.cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
 	{ -- LSP Configuration & Plugins
 		"neovim/nvim-lspconfig",
 		dependencies = {
+			{ "saghen/blink.cmp" },
 			{
 				"williamboman/mason.nvim",
 				-- NOTE: nixCats: use lazyAdd to only enable mason if nix wasnt involved.
@@ -171,7 +172,8 @@ return {
 			--  By default, Neovim doesn't support everything that is in the LSP specification.
 			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			-- config.capabilities =
 			-- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 			-- capabilities =
