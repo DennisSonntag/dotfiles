f8118d1fc9e4001018959101ccc7257372ae9c04
Dennis Sonntag <sonntagdennis8@gmail.com>
2025-01-11 10:33:53 -0700

    switched it cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,177 +1,119 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		optional = true,
-		enabled = false,
-	},
-	{
-		"saghen/blink.cmp",
-		-- version = "*",
-		-- build = vim.g.lazyvim_blink_main and "cargo build --release",
-		build = "nix run .#build-plugin",
-		opts_extend = {
-			"sources.completion.enabled_providers",
-			"sources.compat",
-			"sources.default",
-		},
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			-- add blink.compat to dependencies
-			{
-				"saghen/blink.compat",
-				optional = true, -- make optional so it's only enabled if any extras need it
-				opts = {},
-				version = "*",
-			},
-		},
-		event = "InsertEnter",
+  {
+    "saghen/blink.cmp",
+    enabled = false,
+    optional = true,
+  },
 
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			-- snippets = {
-			-- 	expand = function(snippet, _)
-			-- 		return LazyVim.cmp.expand(snippet)
-			-- 	end,
-			-- },
-			appearance = {
-				-- sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- useful for when your theme doesn't support blink.cmp
-				-- will be removed in a future release, assuming themes add support
-				use_nvim_cmp_as_default = false,
-				-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- adjusts spacing to ensure icons are aligned
-				nerd_font_variant = "mono",
-			},
-			completion = {
-				accept = {
-					-- experimental auto-brackets support
-					auto_brackets = {
-						enabled = true,
-					},
-				},
-				menu = {
-					draw = {
-						treesitter = { "lsp" },
-					},
-				},
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 200,
-				},
-				ghost_text = {
-					enabled = vim.g.ai_cmp,
-				},
-			},
+  -- Setup nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    -- Not all LSP servers add brackets when completing a function.
+    -- To better deal with this, LazyVim adds a custom option to cmp,
+    -- that you can configure. For example:
+    --
+    -- ```lua
+    -- opts = {
+    --   auto_brackets = { "python" }
+    -- }
+    -- ```
+    opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+      local auto_select = true
+      return {
+        auto_brackets = {}, -- configure any filetype to auto add brackets
+        completion = {
+          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          -- ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+          -- ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+          -- ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+          -- ["<tab>"] = function(fallback)
+          --   return LazyVim.cmp.map({ "snippet_forward", "ai_accept" }, fallback)()
+          -- end,
+        }),
+        sources = cmp.config.sources({
+          { name = "lazydev" },
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+        formatting = {
+          format = function(entry, item)
+            -- local icons = LazyVim.config.icons.kinds
+            -- if icons[item.kind] then
+            --   item.kind = icons[item.kind] .. item.kind
+            -- end
 
-			-- experimental signature help support
-			-- signature = { enabled = true },
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
 
-			sources = {
-				-- adding any nvim-cmp sources here will enable them
-				-- with blink.compat
-				compat = {},
-				default = { "lsp", "path", "snippets", "buffer" },
-				cmdline = {},
-			},
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+              end
+            end
 
-			keymap = {
-				preset = "enter",
-				["<C-y>"] = { "select_and_accept" },
-			},
-		},
-		---@param opts blink.cmp.Config | { sources: { compat: string[] } }
-		config = function(_, opts)
-			-- setup compat sources
-			local enabled = opts.sources.default
-			for _, source in ipairs(opts.sources.compat or {}) do
-				opts.sources.providers[source] = vim.tbl_deep_extend(
-					"force",
-					{ name = source, module = "blink.compat.source" },
-					opts.sources.providers[source] or {}
-				)
-				if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
-					table.insert(enabled, source)
-				end
-			end
+            return item
+          end,
+        },
+        experimental = {
+          -- only show ghost text when we show ai completions
+          ghost_text = vim.g.ai_cmp and {
+            hl_group = "CmpGhostText",
+          } or false,
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+    main = "lazyvim.util.cmp",
+  },
 
-			-- add ai_accept to <Tab> key
-			-- if not opts.keymap["<Tab>"] then
-			-- 	if opts.keymap.preset == "super-tab" then -- super-tab
-			-- 		opts.keymap["<Tab>"] = {
-			-- 			require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
-			-- 			LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-			-- 			"fallback",
-			-- 		}
-			-- 	else -- other presets
-			-- 		opts.keymap["<Tab>"] = {
-			-- 			LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-			-- 			"fallback",
-			-- 		}
-			-- 	end
-			-- end
-
-			-- Unset custom prop to pass blink.cmp validation
-			opts.sources.compat = nil
-
-			-- check if we need to override symbol kinds
-			for _, provider in pairs(opts.sources.providers or {}) do
-				---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
-				if provider.kind then
-					local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-					local kind_idx = #CompletionItemKind + 1
-
-					CompletionItemKind[kind_idx] = provider.kind
-					---@diagnostic disable-next-line: no-unknown
-					CompletionItemKind[provider.kind] = kind_idx
-
-					---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
-					local transform_items = provider.transform_items
-					---@param ctx blink.cmp.Context
-					---@param items blink.cmp.CompletionItem[]
-					provider.transform_items = function(ctx, items)
-						items = transform_items and transform_items(ctx, items) or items
-						for _, item in ipairs(items) do
-							item.kind = kind_idx or item.kind
-						end
-						return items
-					end
-
-					-- Unset custom prop to pass blink.cmp validation
-					provider.kind = nil
-				end
-			end
-
-			require("blink.cmp").setup(opts)
-		end,
-	},
-
-	-- add icons
-	{
-		"saghen/blink.cmp",
-		opts = function(_, opts)
-			-- opts.appearance = opts.appearance or {}
-			-- opts.appearance.kind_icons = vim.tbl_extend("keep", {
-			-- 	Color = "██", -- Use block instead of icon for color items to make swatches more usable
-			-- }, LazyVim.config.icons.kinds)
-		end,
-	},
-
-	-- lazydev
-	{
-		"saghen/blink.cmp",
-		opts = {
-			sources = {
-				-- add lazydev to your completion providers
-				default = { "lazydev" },
-				providers = {
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						score_offset = 100, -- show at a higher priority than lsp
-					},
-				},
-			},
-		},
-	},
+  -- snippets
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "garymjr/nvim-snippets",
+        opts = {
+          friendly_snippets = true,
+        },
+        dependencies = { "rafamadriz/friendly-snippets" },
+      },
+    },
+    			config = true,
+    -- opts = function(_, opts)
+    --   opts.snippet = {
+    --     expand = function(item)
+    --       return LazyVim.cmp.expand(item.body)
+    --     end,
+    --   }
+    --   if LazyVim.has("nvim-snippets") then
+    --     table.insert(opts.sources, { name = "snippets" })
+    --   end
+    -- end,
+  },
 }
