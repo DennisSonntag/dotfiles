c0b2f38689d863bc5df3c388535637eae4618b99
Dennis Sonntag <sonntagdennis8@gmail.com>
2025-07-02 12:31:30 -0600

    changed the way i do the clangd thing

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -251,19 +251,20 @@ return {
 				-- },
 				cmd = {
 					"clangd",
-					"--std=c23",
-				-- 	"--background-index",
-				-- 	"--clang-tidy",
-				-- 	"--header-insertion=iwyu",
-				-- 	"--completion-style=detailed",
-				-- 	"--function-arg-placeholders",
-				-- 	"--fallback-style=llvm",
+					-- "--std=c23",
+					-- 	"--background-index",
+					-- 	"--clang-tidy",
+					-- 	"--header-insertion=iwyu",
+					-- 	"--completion-style=detailed",
+					-- 	"--function-arg-placeholders",
+					-- 	"--fallback-style=llvm",
 				},
-				-- init_options = {
-				-- 	usePlaceholders = true,
-				-- 	completeUnimported = true,
-				-- 	clangdFileStatus = true,
-				-- },
+				init_options = {
+					fallbackFlags = { "--std=c23" },
+					-- 	usePlaceholders = true,
+					-- 	completeUnimported = true,
+					-- 	clangdFileStatus = true,
+				},
 			}
 
 			servers.lua_ls = {
